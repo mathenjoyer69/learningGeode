58db3e0d1f4c8d79ff78c91804f0d65f77e1888c
ariel <arielcalif10@gmail.com>
2025-11-03 16:04:39 +0200

    added pong cuz idk what to do

--- src/layers/YolaLayer.cpp
+++ src/layers/YolaLayer.cpp
@@ -1,4 +1,5 @@
 #include "YolaLayer.hpp"
+#include <random>
 
 YolaLayer* YolaLayer::sharedInstance = nullptr;
 
@@ -20,8 +21,9 @@ bool YolaLayer::init() {
     bgMenu->setPosition({0, 0});
     this->addChild(bgMenu);
     auto bgSprite = CCSprite::create("yolapork.png"_spr);
-    auto winSize = CCDirector::sharedDirector()->getWinSize();
-    bgSprite->setPosition({winSize.width / 2, winSize.height / 2});
+    m_winSize  = CCDirector::sharedDirector()->getWinSize();
+
+    bgSprite->setPosition({m_winSize .width / 2, m_winSize .height / 2});
     bgSprite->setScaleY(4.0f);
     bgSprite->setScaleX(11.5f);
     bgSprite->setID("bg-menu");
@@ -48,15 +50,26 @@ bool YolaLayer::init() {
     auto pongMenu = CCMenu::create();
     pongMenu->setPosition({0, 0});
     this->addChild(pongMenu);
-    auto pongSprite1 = CCSprite::create("yolapork.png"_spr);
-    pongSprite1->setPosition({winSize.width-50, winSize.height / 2});
-    pongSprite1->setColor({255, 0, 0});
-    pongMenu->addChild(pongSprite1);
 
-    auto pongSprite2 = CCSprite::create("yolapork.png"_spr);
-    pongSprite2->setPosition({50, winSize.height / 2});
-    pongSprite2->setColor({0, 0, 255});
-    pongMenu->addChild(pongSprite2);
+    m_rightPong = CCSprite::create("yolapork.png"_spr);
+    m_rightPong->setPosition({m_winSize .width-50, m_winSize .height / 2});
+    m_rightPong->setColor({255, 0, 0});
+    pongMenu->addChild(m_rightPong);
+
+    m_leftPong = CCSprite::create("yolapork.png"_spr);
+    m_leftPong->setPosition({50, m_winSize .height / 2});
+    m_leftPong->setColor({0, 0, 255});
+    pongMenu->addChild(m_leftPong);
+
+    m_ball = CCSprite::create("yolapork.png"_spr);
+    m_ball->setPosition({m_winSize .width / 2, m_winSize .height / 2});
+    m_ball->setColor({255, 255, 255});
+    m_ball->setScaleY(0.225f);
+    m_ball->setScaleX(0.4f);
+    pongMenu->addChild(m_ball);
+
+    m_ballVel = CCPoint(250.0f, 150.0f);
+    this->schedule(schedule_selector(YolaLayer::updateBall));
 
     return true;
 }
@@ -64,4 +77,49 @@ bool YolaLayer::init() {
 void YolaLayer::onBackButton(CCObject* sender) {
     CCDirector::sharedDirector()->popSceneWithTransition(0.5f, PopTransition::kPopTransitionFade);
     sharedInstance = nullptr;
+}
+
+void YolaLayer::resetBall(bool right) {
+    m_ball->setPosition({m_winSize.width / 2, m_winSize.height / 2});
+
+    std::random_device rd;
+    std::mt19937 gen(rd());
+    std::uniform_int_distribution<> dis(200, 300);
+    int Velocity = dis(gen);
+    int sign = (rand() % 2 == 0) ? 1 : -1;
+    m_ballVel = CCPoint(right ? Velocity : -1 * Velocity, sign * 150.0f);
+}
+
+void YolaLayer::updateBall(float dt) {
+    auto pos = m_ball->getPosition();
+    pos += m_ballVel * dt;
+    m_ball->setPosition(pos);
+
+    const float radius = 10.0f;
+
+    if (pos.y < radius || pos.y > m_winSize.height - radius)
+        m_ballVel.y *= -1.0f;
+
+    auto rightPos = m_rightPong->getPosition();
+    rightPos.y += (m_ball->getPositionY() - rightPos.y) * 0.05f;
+    m_rightPong->setPosition(rightPos);
+
+    auto leftPos = m_leftPong->getPosition();
+    auto mouseY = getMousePos().y;
+    leftPos.y += (mouseY - leftPos.y) * 0.2f;
+    m_leftPong->setPosition(leftPos);
+
+    auto ballBB = m_ball->boundingBox();
+    auto leftBB = m_leftPong->boundingBox();
+    auto rightBB = m_rightPong->boundingBox();
+
+    if (ballBB.intersectsRect(leftBB) && m_ballVel.x < 0) {
+        m_ballVel.x *= -1.0f;
+    }
+    if (ballBB.intersectsRect(rightBB) && m_ballVel.x > 0) {
+        m_ballVel.x *= -1.0f;
+    }
+
+    if (pos.x < 0 || pos.x > m_winSize.width)
+        resetBall(pos.x > m_winSize.width);
 }
--- src/layers/YolaLayer.hpp
+++ src/layers/YolaLayer.hpp
@@ -6,9 +6,19 @@ using namespace geode::prelude;
 
 class YolaLayer : public CCLayer {
     public:
+        CCSprite* m_leftPong;
+        CCSprite* m_rightPong ;
+        CCSprite* m_ball;
+        
+        CCPoint m_ballVel;
+        CCSize m_winSize;
+
+        void updateBall(float dt);
+        void resetBall(bool right);
+
         static YolaLayer* create();
         void onBackButton(CCObject*);
-
+        
     private:
         virtual bool init();
         static YolaLayer* sharedInstance;
